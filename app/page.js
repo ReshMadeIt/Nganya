4430ff6be53aee83e7ad8876c8336f35c435a01d
ReshMadeIt <reshmadeit@gmail.com>
2026-02-06 18:25:18 +0300

    Update Home component and replace script tag
    Refactor Home component to use Next.js Script component for LinkedIn script.

--- app/page.js
+++ app/page.js
@@ -1,17 +1,35 @@
+import Script from "next/script";
 
-export default async function Home() {
+export default function Home() {
   return (
-    <main style={{ fontFamily: "sans-serif", textAlign: "center", padding: "50px" }}>
+    <main
+      style={{
+        fontFamily: "sans-serif",
+        textAlign: "center",
+        padding: "50px",
+      }}
+    >
       <img
         src="/nganyaa.png"
         alt="Nganya App Icon"
-        style={{ width: "150px", height: "150px", borderRadius: "20%", marginBottom: "20px", objectFit: "cover" }}
+        style={{
+          width: "150px",
+          height: "150px",
+          borderRadius: "20%",
+          marginBottom: "20px",
+          objectFit: "cover",
+        }}
       />
+
       <h1>Nganya</h1>
       <p>Your ultimate matatu culture app – now on Google Play!</p>
 
-<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
- 
+      {/* LinkedIn script */}
+      <Script
+        src="https://platform.linkedin.com/badges/js/profile.js"
+        strategy="afterInteractive"
+      />
+
       <a
         href="https://play.google.com/store/apps/details?id=com.nganya.mobile"
         target="_blank"
