b25d3e4c4f5bdb318a4ae138017848671878ed8a
ReshMadeIt <reshmadeit@gmail.com>
2026-03-22 16:53:09 +0300

    Updates

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,20 @@
 // components/Navbar.tsx
 import Link from 'next/link';
 
+const navItems = [
+    { name: "Home", href: "/" },
+    { name: "About", href: "/about" },
+    { name: "News", href: "/news" },
+    { name: "Contact", href: "/contact" },
+  ];
+
 const Navbar = () => {
   return (
+  
+  
+  
     <nav style={{ display: 'flex', justifyContent: 'space-around', padding: '1rem', borderBottom: '1px solid #ccc' }}>
-      <Link href="/">
+     /* <Link href="/">
         Home
       </Link>
       <Link href="/about">
@@ -12,7 +22,41 @@ const Navbar = () => {
       </Link>
       <Link href="/contact">
         Contact
-      </Link>
+      </Link>*/
+      
+      <div className="container flex flex-wrap items-center justify-between mx-auto text-slate-800">
+          <Link
+            href="/"
+            className="mr-4 block cursor-pointer py-1.5 text-red-600 font-bold text-2xl"
+          >
+            Nganya254
+          </Link>
+
+          <div className="lg:hidden">
+            <button
+              className="relative ml-auto h-6 max-h-[40px] w-6 max-w-[40px] select-none rounded-lg text-center align-middle text-xs font-medium uppercase text-inherit transition-all hover:bg-transparent focus:bg-transparent active:bg-transparent disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              onClick={/*toggleMobileMenu*/}
+              type="button"
+            >
+              <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-8 h-8"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </span>
+            </button>
+          </div>
+          </div>
+      
     </nav>
   );
 };
