43288c20bd0c2c78609374a0da05c6282e8d063d
ReshMadeIt <reshmadeit@gmail.com>
2026-03-22 16:55:40 +0300

    Updates

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,49 +14,12 @@ const Navbar = () => {
   
   
     <nav style={{ display: 'flex', justifyContent: 'space-around', padding: '1rem', borderBottom: '1px solid #ccc' }}>
-     /* <Link href="/">
-        Home
+      <Link href="/"
+      className="mr-4 block cursor-pointer py-1.5 text-red-600 font-bold text-2xl">
+        Nganya254
       </Link>
-      <Link href="/about">
-        About
-      </Link>
-      <Link href="/contact">
-        Contact
-      </Link>*/
       
-      <div className="container flex flex-wrap items-center justify-between mx-auto text-slate-800">
-          <Link
-            href="/"
-            className="mr-4 block cursor-pointer py-1.5 text-red-600 font-bold text-2xl"
-          >
-            Nganya254
-          </Link>
-
-          <div className="lg:hidden">
-            <button
-              className="relative ml-auto h-6 max-h-[40px] w-6 max-w-[40px] select-none rounded-lg text-center align-middle text-xs font-medium uppercase text-inherit transition-all hover:bg-transparent focus:bg-transparent active:bg-transparent disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-              onClick={/*toggleMobileMenu*/}
-              type="button"
-            >
-              <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-8 h-8"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  ></path>
-                </svg>
-              </span>
-            </button>
-          </div>
-          </div>
-      
+     
     </nav>
   );
 };
